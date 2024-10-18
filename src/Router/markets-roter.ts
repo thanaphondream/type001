afd2dd0baeef947a74ef7ae2932bd4e631c9d77e
DREAM <voigkgky@gmail.com>
2024-10-18 18:12:36 +0700

    Save 06

--- src/Router/markets-roter.ts
+++ src/Router/markets-roter.ts
@@ -4,8 +4,8 @@ import { authenticates } from '../auth/authenticate';
 import * as stall from '../work-all/stall-Markets'
 
 router.post('/markets', stall.markets )
-
 router.get('/markets', stall.marketdatashow)
 router.get('/markets/:id', stall.marketshowid)
+router.put('/markets/:id', stall.marketsupdate)
 
 export default router;
