4cfa081570bcfbd3b994e2bb16ff3b928bbb29a2
DREAM <voigkgky@gmail.com>
2024-11-11 00:08:22 +0700

    first commit

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,24 +1,18 @@
 import multer, { StorageEngine } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
-import fs from 'fs';
 
-// ตรวจสอบและสร้างโฟลเดอร์ 'uploads' ถ้ายังไม่มี
-const uploadFolder = 'uploads';
-if (!fs.existsSync(uploadFolder)) {
-    fs.mkdirSync(uploadFolder);
-}
 
 const storage: StorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, uploadFolder); 
+        cb(null, 'uploads'); 
     },
     filename: (req, file, cb) => {
         const extension = path.extname(file.originalname);
-        cb(null, uuidv4() + "." + file.mimetype.split("/")[1]); 
+        cb(null, uuidv4() + "."+file.mimetype.split("/")[1]); 
     }
 });
 
 const upload = multer({ storage });
 
-export default upload
+export default upload;
