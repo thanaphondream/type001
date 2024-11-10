9d766889b3c809849d72d5acd7bf9aaf84088d7c
DREAM <voigkgky@gmail.com>
2024-11-10 22:49:00 +0700

    Save day 17

--- src/Router/Paymet-roter.ts
+++ src/Router/Paymet-roter.ts
@@ -4,7 +4,7 @@ import { authenticates } from '../auth/authenticate';
 import * as paymet from '../work-all/stall-Payment'
 import upload from '../middlewares/upload'
 
-router.post('/paymetbn', upload.array('payment_image', 1), paymet.payment_model, paymet.payment_save)
+router.post('/paymet', upload.array('payment_image', 1), paymet.payment_model, paymet.payment_save)
 router.put('/paymet/:paymet', authenticates, upload.array('payment_image', 2), paymet.payment_model, paymet.paymet_Id, paymet.Paymet_Update)
 router.get('/payment/:paymet', authenticates, paymet.paymet_Id, paymet.payment_show)
 router.get('/paymets', paymet.paymet_ShowAll)
