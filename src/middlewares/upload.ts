c862599b8f5e70a9bc8227a1309f4608709cba42
DREAM <voigkgky@gmail.com>
2024-11-11 19:00:44 +0700

    Save 18

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,15 +1,6 @@
-import multer, { StorageEngine } from 'multer';
-import { v4 as uuidv4 } from 'uuid';
-import path from 'path';
+import multer from 'multer';
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        cb(null, uuidv4() + "."+file.mimetype.split("/")[1]);
-    }
-});
+const storage = multer.memoryStorage(); // ใช้ memoryStorage แทน diskStorage
 
 const upload = multer({ storage });
 
